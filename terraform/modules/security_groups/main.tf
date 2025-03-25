b2943fc6f679b7b4ab2977d1154bab101d98bc2a
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-03-25 14:36:37 +0530

    Updated main.tf

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -79,6 +79,20 @@ resource "aws_security_group" "elastic_sg" {
     security_groups = [aws_security_group.lb_sg.id, aws_security_group.jumphost_sg.id]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -101,6 +115,20 @@ resource "aws_security_group" "k_sg" {
     to_port         = 5600
     protocol        = "tcp"
     security_groups = [aws_security_group.lb_sg.id, aws_security_group.jumphost_sg.id]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
