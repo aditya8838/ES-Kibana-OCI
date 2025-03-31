b4f760fa968a9724974dd9d6782b25adde71885d
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-03-31 21:38:03 +0530

    Update variables.tf

--- terraform/modules/security_groups/variables.tf
+++ terraform/modules/security_groups/variables.tf
@@ -2,3 +2,23 @@ variable "vpc_id" {
   description = "ID of the VPC"
   type        = string
 }
+
+variable "elasticsearch1_private_ip" {
+  description = "Private IP of Elasticsearch Node 1 (e.g., 192.168.1.10)"
+  type        = string
+}
+
+variable "elasticsearch2_private_ip" {
+  description = "Private IP of Elasticsearch Node 2"
+  type        = string
+}
+
+variable "elasticsearch3_private_ip" {
+  description = "Private IP of Elasticsearch Node 3"
+  type        = string
+}
+
+variable "elasticsearch4_private_ip" {
+  description = "Private IP of Elasticsearch Node 4"
+  type        = string
+}
