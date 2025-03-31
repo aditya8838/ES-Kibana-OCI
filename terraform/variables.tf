7dc3414e19d842f46132b306db8fb1f8b6b465cd
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-03-31 22:54:21 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+# VPC Configuration
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
@@ -10,6 +11,7 @@ variable "vpc_name" {
   default     = "project-vpc"
 }
 
+# Availability Zones
 variable "az1" {
   description = "First availability zone"
   type        = string
@@ -22,30 +24,41 @@ variable "az2" {
   default     = "ap-south-1b"
 }
 
+# Internet Gateway
 variable "igw_name" {
   description = "Name tag for the Internet Gateway"
   type        = string
   default     = "project-igw"
 }
 
+# NAT Gateway
 variable "nat_gateway_name" {
   description = "Name tag for the NAT Gateway"
   type        = string
   default     = "project-natgw"
 }
 
+# VPC Peering
 variable "peering_name" {
   description = "Name tag for the peering connection"
   type        = string
   default     = "project-peering"
 }
 
+variable "demo_vpc_id" {
+  description = "ID of the Demo-VPC for peering"
+  type        = string
+  default     = "vpc-0996ede71c3cf9ec2"
+}
+
+# Load Balancer
 variable "alb_name" {
   description = "Name of the Application Load Balancer"
   type        = string
   default     = "project-lb"
 }
 
+# EC2 Instances
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
@@ -62,4 +75,31 @@ variable "key_name" {
   description = "Key pair name for the EC2 instances"
   type        = string
   default     = "testkey"
+}
+
+variable "key_file_path" {
+  description = "Path to the private key file for EC2 instances"
+  type        = string
+  default     = "/var/lib/jenkins/workspace/testkey.pem"
+}
+
+# Elasticsearch Security Group Rules
+variable "elasticsearch1_private_ip" {
+  description = "Private IP of Elasticsearch Node 1 (format: '192.168.1.10')"
+  type        = string
+}
+
+variable "elasticsearch2_private_ip" {
+  description = "Private IP of Elasticsearch Node 2"
+  type        = string
+}
+
+variable "elasticsearch3_private_ip" {
+  description = "Private IP of Elasticsearch Node 3"
+  type        = string
+}
+
+variable "elasticsearch4_private_ip" {
+  description = "Private IP of Elasticsearch Node 4"
+  type        = string
 }
