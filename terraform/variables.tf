bc5d72a3dacafdbf8a033f36304bb5ec81810738
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-03-31 23:14:59 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -82,24 +82,3 @@ variable "key_file_path" {
   type        = string
   default     = "/var/lib/jenkins/workspace/testkey.pem"
 }
-
-# Elasticsearch Security Group Rules
-variable "elasticsearch1_private_ip" {
-  description = "Private IP of Elasticsearch Node 1 (format: '192.168.1.10')"
-  type        = string
-}
-
-variable "elasticsearch2_private_ip" {
-  description = "Private IP of Elasticsearch Node 2"
-  type        = string
-}
-
-variable "elasticsearch3_private_ip" {
-  description = "Private IP of Elasticsearch Node 3"
-  type        = string
-}
-
-variable "elasticsearch4_private_ip" {
-  description = "Private IP of Elasticsearch Node 4"
-  type        = string
-}
