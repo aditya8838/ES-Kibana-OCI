ae2630abba096cfc189addb7efeafb1c4324cbcf
Aditya Tripathi <121555179+aditya8838@users.noreply.github.com>
2025-03-31 22:18:46 +0530

    Update main.tf

--- terraform/modules/route_table/main.tf
+++ terraform/modules/route_table/main.tf
@@ -1,16 +1,48 @@
+# Lookup Demo-VPC's public route table by tag
+data "aws_route_table" "demo_pub_rt" {
+  vpc_id = var.demo_vpc_id
+  filter {
+    name   = "tag:Name"
+    values = ["Pub-RT"]  # Must match the tag in Demo-VPC
+  }
+}
+
+# Your project VPC's public route table
 resource "aws_route_table" "public_rt" {
   vpc_id = var.vpc_id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = var.igw_id
   }
-
   tags = {
     Name = "public-rt"
   }
 }
 
+# Your project VPC's private route table
+resource "aws_route_table" "private_rt" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = var.nat_gateway_id
+  }
+  route {
+    cidr_block                = "10.0.0.0/16"  # Route to Demo-VPC
+    vpc_peering_connection_id = var.peering_connection_id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Add reciprocal route to Demo-VPC's public route table
+resource "aws_route" "demo_vpc_route" {
+  route_table_id            = data.aws_route_table.demo_pub_rt.id
+  destination_cidr_block    = "192.168.0.0/16"  # Your VPC's CIDR
+  vpc_peering_connection_id = var.peering_connection_id
+}
+
+# Route table associations (unchanged)
 resource "aws_route_table_association" "public1" {
   subnet_id      = var.public_subnet1_id
   route_table_id = aws_route_table.public_rt.id
@@ -19,25 +51,6 @@ resource "aws_route_table_association" "public1" {
 resource "aws_route_table_association" "public2" {
   subnet_id      = var.public_subnet2_id
   route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = var.vpc_id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = var.nat_gateway_id
-  }
-
-  # New route for VPC peering
-  route {
-    cidr_block                = "10.0.0.0/16"  # Demo-VPC CIDR
-    vpc_peering_connection_id = var.peering_connection_id
-  }
-
-  tags = {
-    Name = "private-rt"
-  }
 }
 
 resource "aws_route_table_association" "private1" {
